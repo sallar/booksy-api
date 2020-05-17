bd3e5776527415aaff3385c4c6ea69e6e1e4db95
Sallar Kaboli <sallar@kaboli.org>
2020-05-17 16:09:24 +0300

    Fix an issue with book creation

--- api/add-book.ts
+++ api/add-book.ts
@@ -4,8 +4,10 @@ import { getOrCreateGoogleBook, GoogleBook } from "./_utils/db";
 export default async (req: NowRequest, res: NowResponse) => {
   try {
     const book: GoogleBook = req.body;
+    if (!book || !book.id) {
+      throw new Error("No book provided.");
+    }
     const id = await getOrCreateGoogleBook(book);
-
     res.json({ success: true, id });
   } catch (err) {
     console.error(err);
